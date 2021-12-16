2a29fc595f6954590054df5c7cac9616261988cc
TrevorRezac <trev.rezac@gmail.com>
2021-12-15 17:51:30 -0800

    second passing TDD

--- test/example.test.js
+++ test/example.test.js
@@ -1,7 +1,7 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
-import { renderGame } from '../render-utils.js';
+import { renderGame, renderTeam } from '../render-utils.js';
 
 const test = QUnit.test;
 
@@ -19,6 +19,23 @@ test('renderGame(game) should take in a game object and return DOM elements cont
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = renderGame(game);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
+
+test('renderTeam(name, score) should take in a name and a score and return DOM elements containing the name and score', (expect) => {
+    //Arrange
+    
+    let name = 'Trevor';
+    let score = 10;
+    // Set up your arguments and expectations
+    const expected = '<div class="team"><p class="name">Trevor</p><p class="score">10</p></div>';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderTeam(name, score);
 
     //Expect
     // Make assertions about what is expected versus the actual result
